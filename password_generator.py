2125aece046704a21e28a32f683476ba6eb12df4
Thomas <163894641+TommyGarfield@users.noreply.github.com>
2025-05-07 12:45:15 +0200

    added strings containing characters that can be used in password.

--- password_generator.py
+++ password_generator.py
@@ -3,5 +3,12 @@
 #Get password length from user, store in variable password_length
 password_length = int(input("Enter the desired password length "))
 
-#checking that variable is stored
-print(password_length)
+#variables containing characteres that can be used to create a password
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '0123456789'
+symbols = '!@#£%^&*()~-_'
+
+
+
+
