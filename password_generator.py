c38eb3b3f8b4e94e39bc6b79a113e6f42c352678
Thomas <163894641+TommyGarfield@users.noreply.github.com>
2025-05-07 12:28:55 +0200

    Ask user for desired passwrd length

---
+++ password_generator.py
@@ -0,0 +1,7 @@
+#Password generator
+
+#Get password length from user, store in variable password_length
+password_length = int(input("Enter the desired password length "))
+
+#checking that variable is stored
+print(password_length)
